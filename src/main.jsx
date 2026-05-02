26953bb6e8af5fc60442957119a776c5c4a09873
muslih2021 <sahmatmuslih@gmail.com>
2026-05-02 13:19:07 +0800

    add vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Analytics } from '@vercel/analytics/react'
 
 window.onerror = (msg, url, line, col, error) => {
   console.error("[Global Error]:", { msg, url, line, col, error });
@@ -12,8 +13,10 @@ window.onunhandledrejection = (event) => {
   console.error("[Unhandled Promise Rejection]:", event.reason);
 };
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
 )
